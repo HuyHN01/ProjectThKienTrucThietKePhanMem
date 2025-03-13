f7488894a3e8d25be7a41b1c2b9e28860482e1d6
HuyHN01 <167284534+HuyHN01@users.noreply.github.com>
2025-03-13 15:23:30 +0700

    LoginPage

---
+++ ASC.Solution/ASC.Web/Services/DependencyInjection.cs
@@ -0,0 +1,60 @@
+﻿using ASC.DataAccess.Interfaces;
+using ASC.DataAccess;
+using ASC.Web.Configuration;
+using ASC.Web.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASC.Web.Services
+{
+    public static class DependencyInjection
+    {
+        // Cấu hình các dịch vụ
+        public static IServiceCollection AddConfig(this IServiceCollection services, IConfiguration config)
+        {
+            // Thêm AddDbContext với connectionString để kết nối cơ sở dữ liệu
+            var connectionString = config.GetConnectionString("DefaultConnection")
+                ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+
+            // Thêm các dịch vụ Options và lấy dữ liệu từ appsettings.json với "AppSettings"
+            services.AddOptions(); // IOOption
+            services.Configure<ApplicationSettings>(config.GetSection("AppSettings"));
+
+            return services;
+        }
+
+        // Cấu hình các dịch vụ
+        public static IServiceCollection AddMyDependencyGroup(this IServiceCollection services)
+        {
+            // Thêm ApplicationDbContext
+            services.AddScoped<DbContext, ApplicationDbContext>();
+
+            // Cấu hình IdentityUser và IdentityRole
+            services.AddIdentity<IdentityUser, IdentityRole>(options =>
+            {
+                options.User.RequireUniqueEmail = true;
+            })
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
+
+            // Thêm các dịch vụ khác
+            services.AddTransient<IEmailSender, AuthMessageSender>();
+            services.AddTransient<ISmsSender, AuthMessageSender>();
+            services.AddSingleton<IIdentitySeed, IdentitySeed>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            //Add cache, session
+            services.AddSession();
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            // Thêm RazorPages và MVC
+            services.AddRazorPages();
+            services.AddDatabaseDeveloperPageExceptionFilter();
+            services.AddControllersWithViews();
+
+
+            return services;
+        }
+    }
+}
