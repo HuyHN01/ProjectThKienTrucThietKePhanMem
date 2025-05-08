eaabd6d5c01f382053994a69b0f1b523a0fe450f
HuyHN01 <167284534+HuyHN01@users.noreply.github.com>
2025-05-08 16:12:30 +0700

    Chưa tạo được New Service Request

--- ASC.Solution/ASC.Business/ServiceRequestOperations.cs
+++ ASC.Solution/ASC.Business/ServiceRequestOperations.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ASC.Business.Interfaces;
+using ASC.DataAccess.Interfaces;
+using ASC.Model.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,45 @@ using System.Threading.Tasks;
 
 namespace ASC.Business
 {
-    internal class ServiceRequestOperations
+    public class ServiceRequestOperations : IServiceRequestOperations
     {
+        private readonly IUnitOfWork _unitOfWork;
+        public ServiceRequestOperations (IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task CreateServiceRequestAsync(ServiceRequest request)
+        {
+            using (_unitOfWork)
+            {
+                await _unitOfWork.Repository<ServiceRequest>().AddAsync(request);
+                _unitOfWork.CommitTransaction();
+            }
+        }
+
+        public ServiceRequest UpdateServiceRequest(ServiceRequest request)
+        {
+            using (_unitOfWork)
+            {
+                _unitOfWork.Repository<ServiceRequest>().Update(request);
+                _unitOfWork.CommitTransaction();
+                return request;
+            }
+        }
+
+        public async Task<ServiceRequest> UpdateServiceRequestStatusAsync(string rowKey, string partitionKey, string status)
+        {
+            using (_unitOfWork)
+            {
+                var serviceRequest = await _unitOfWork.Repository<ServiceRequest>().FindAsync(partitionKey, rowKey);
+                if (serviceRequest == null)
+                    throw new NullReferenceException();
+                serviceRequest.Status = status;
+                _unitOfWork.Repository<ServiceRequest>().Update(serviceRequest);
+                _unitOfWork.CommitTransaction();
+                return serviceRequest;
+            }
+        }
     }
 }
